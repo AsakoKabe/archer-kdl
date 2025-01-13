3822b92a7108b29641800582bdb7edfc6a064c75
Denis Mamatin <some@some>
2025-01-13 11:30:48 +0300

    fix delete links

--- tasklist-glsp-server/src/handler/delete-element-handler.ts
+++ tasklist-glsp-server/src/handler/delete-element-handler.ts
@@ -25,7 +25,7 @@ import {
     toTypeGuard
 } from '@eclipse-glsp/server';
 import { inject, injectable } from 'inversify';
-import { Cluster, Container, Ingress, KDLBaseElement, Pod, Port, Service } from '../model/tasklist-model';
+import { Cluster, Container, Ingress, KDLBaseElement, Link, Pod, Port, Service } from '../model/tasklist-model';
 import { TaskListModelState } from '../model/tasklist-model-state';
 
 @injectable()
@@ -70,11 +70,20 @@ export class DeleteElementHandler extends JsonOperationHandler {
         return [];
     }
 
+    private deleteLinks(element: KDLBaseElement): void{
+        this.modelState.sourceModel.links.forEach(link => {
+            if (link.sourceId === element.id || link.targetId === element.id) {
+                remove(this.modelState.sourceModel.links, link);
+            }
+        });
+    }
+
     private deleteIngress(ingress: Ingress): void {
         remove(this.modelState.sourceModel.ingresses, ingress);
         this.modelState.sourceModel.clusters.forEach(cluster => {
             cluster.ingress_ids = cluster.ingress_ids.filter(id => id !== ingress.id);
         });
+        this.deleteLinks(ingress);
     }
 
     private deletePod(pod: Pod): void {
@@ -90,6 +99,7 @@ export class DeleteElementHandler extends JsonOperationHandler {
         this.modelState.sourceModel.clusters.forEach(cluster => {
             cluster.pod_ids = cluster.pod_ids.filter(id => id !== pod.id);
         });
+        this.deleteLinks(pod);
     }
 
     private deleteService(service: Service): void {
@@ -101,6 +111,7 @@ export class DeleteElementHandler extends JsonOperationHandler {
         this.modelState.sourceModel.clusters.forEach(cluster => {
             cluster.service_ids = cluster.service_ids.filter(id => id !== service.id);
         });
+        this.deleteLinks(service);
     }
 
     private deleteContainer(container: Container): void {
@@ -108,6 +119,7 @@ export class DeleteElementHandler extends JsonOperationHandler {
         this.modelState.sourceModel.pods.forEach(pod => {
             pod.container_ids = pod.container_ids.filter(id => id !== container.id);
         });
+        this.deleteLinks(container);
     }
 
     private deletePort(port: Port): void {
@@ -118,6 +130,7 @@ export class DeleteElementHandler extends JsonOperationHandler {
         this.modelState.sourceModel.services.forEach(service => {
             service.port_ids = service.port_ids.filter(id => id !== port.id);
         });
+        this.deleteLinks(port);
     }
 
     private deleteCluster(cluster: Cluster): void {
@@ -125,6 +138,7 @@ export class DeleteElementHandler extends JsonOperationHandler {
             this.deleteModelElement(this.modelState.index.findElement(id));
         });
         remove(this.modelState.sourceModel.clusters, cluster);
+        this.deleteLinks(cluster);
     }
 
     private deleteModelElement(modelElement: KDLBaseElement | undefined): void {
@@ -140,6 +154,8 @@ export class DeleteElementHandler extends JsonOperationHandler {
             this.deleteContainer(modelElement);
         } else if (Port.is(modelElement)) {
             this.deletePort(modelElement);
+        } else if (Link.is(modelElement)) {
+            remove(this.modelState.sourceModel.links, modelElement);
         }
     }
 }
